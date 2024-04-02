e0efb822816c2a95c2ee18a48b594fffa9635893
Ivanka Alan <helloivankalan@gmail.com>
2024-04-02 23:38:02 +0700

    add getpods function

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 
 bot_token = os.environ.get('bot_token')
 
@@ -10,13 +10,29 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+#handle /getpods command
+async def get_pods(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pods = os.popen("kubectl get pod -n unified").read()
+    await update.message.reply_text(pods)
 
-if __name__ == '__main__':
+#handle /getpods message
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.text == "/getpods":
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=get_pods
+        )
+
+def main():
     application = ApplicationBuilder().token(bot_token).build()
     
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    get_pods_handler = CommandHandler('getpods', get_pods)
+    message_handler = MessageHandler(filters.Text, handle_message)
+
+    application.add_handler(get_pods_handler)
+    application.add_handler(message_handler)
     
     application.run_polling()
+
+if __name__ == '__main__':
+    main()
